b0ac1285a9c20bc78f1489092f165407db12dfc4
mkay <mkay@gmail.com>
2025-05-25 23:22:37 +0300

    Create database tables manually using SQLite CLI

---
+++ lib/db/connection.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('articles.db')
+    conn.row_factory = sqlite3.Row  # This allows us to access columns by name
+    return conn
+
